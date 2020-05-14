df710512060992edef4cc79940d8590b2cf58177
Amaterasus <skyetay03@googlemail.com>
2020-05-14 16:51:56 +0100

    validating users and sending tokens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,17 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
 
         if user && user.authenticate(params[:password])
-            render json: {id: user.id, username: user.username, bio: user.bio, age: user.age}
+            render json: {id: user.id, username: user.username, bio: user.bio, age: user.age, token: generate_token({id: user.id})}
+        else
+            render json: { message: "login error"}
+        end
+    end
+
+    def validate
+        user = get_user
+        
+        if user
+            render json: {id: user.id, username: user.username, bio: user.bio, age: user.age, token: generate_token({id: user.id})}
         else
             render json: { message: "login error"}
         end
